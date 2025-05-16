8955fa24d4e4bdf20f5f87482d8d47f7961beaa8
JPS Grooves <jamescorvall@pm.me>
2025-05-15 21:28:04 -0400

    Update story.js

--- js/story.js
+++ js/story.js
@@ -1,1 +1,11 @@
+function loadStoryChapter(chapterName) {
+  fetch(`/dlc/${chapterName}.json`)
+    .then(res => res.json())
+    .then(data => {
+      loadStory(data); // or renderPanels(data), whatever your display engine is
+    })
+    .catch(err => {
+      console.error(`Error loading ${chapterName}:`, err);
+    });
+}
 
