d62b17cd674d2af79efc9d5a1f48a2df49640ae2
JPS Grooves <jamescorvall@pm.me>
2025-05-31 10:24:34 -0400

    Update sound.js

--- js/sound.js
+++ js/sound.js
@@ -1,24 +1,28 @@
+// /js/sound.js
+
 function setupVolumeSliders() {
   const musicSlider = document.getElementById('musicVolume');
   const sfxSlider = document.getElementById('sfxVolume');
 
   if (!musicSlider || !sfxSlider) return;
 
+  // Set initial values from settings
   musicSlider.value = getSetting('musicVolume') ?? 1;
   sfxSlider.value = getSetting('sfxVolume') ?? 1;
 
+  // Music slider change
   musicSlider.addEventListener('input', (e) => {
     const vol = parseFloat(e.target.value);
     setSetting('musicVolume', vol);
-    setMusicVolume(vol);
+    if (typeof setMusicVolume === 'function') setMusicVolume(vol);
   });
 
+  // SFX slider change
   sfxSlider.addEventListener('input', (e) => {
     const vol = parseFloat(e.target.value);
     setSetting('sfxVolume', vol);
-    setSFXVolume(vol);
+    if (typeof setSFXVolume === 'function') setSFXVolume(vol);
   });
 }
 
 window.setupVolumeSliders = setupVolumeSliders;
-
