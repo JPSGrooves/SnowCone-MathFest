a3bb34c7d34a6ea19ade5edab77124867992f2f6
JPS Grooves <jamescorvall@pm.me>
2025-11-17 13:44:22 -0500

    🍧 Live deploy

--- assets/antAttack-CyaWlQIp.js
+++ assets/antAttack-CyaWlQIp.js
@@ -1,4 +1,4 @@
-import{a as cn}from"./index-DZvMM0Kw.js";function Pt(a){if(a===void 0)throw new ReferenceError("this hasn't been initialised - super() hasn't been called");return a}function ur(a,t){a.prototype=Object.create(t.prototype),a.prototype.constructor=a,a.__proto__=t}/*!
+import{a as cn}from"./index-BknYX7sS.js";function Pt(a){if(a===void 0)throw new ReferenceError("this hasn't been initialised - super() hasn't been called");return a}function ur(a,t){a.prototype=Object.create(t.prototype),a.prototype.constructor=a,a.__proto__=t}/*!
  * GSAP 3.13.0
  * https://gsap.com
  *
