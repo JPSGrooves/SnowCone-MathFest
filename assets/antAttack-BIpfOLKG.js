4d2a31f273490a1f5f2090d7ffd2f8872ac7dc10
JPS Grooves <jamescorvall@pm.me>
2025-12-11 22:18:26 -0500

    🍧 Live deploy

--- assets/antAttack-BIpfOLKG.js
+++ assets/antAttack-BIpfOLKG.js
@@ -1,4 +1,4 @@
-import{a as cn}from"./index-B2qqcBx0.js";function Pt(a){if(a===void 0)throw new ReferenceError("this hasn't been initialised - super() hasn't been called");return a}function ur(a,t){a.prototype=Object.create(t.prototype),a.prototype.constructor=a,a.__proto__=t}/*!
+import{a as cn}from"./index-Lf4neHDN.js";function Pt(a){if(a===void 0)throw new ReferenceError("this hasn't been initialised - super() hasn't been called");return a}function ur(a,t){a.prototype=Object.create(t.prototype),a.prototype.constructor=a,a.__proto__=t}/*!
  * GSAP 3.13.0
  * https://gsap.com
  *
