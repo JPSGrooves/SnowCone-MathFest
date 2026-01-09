02d0ccc86c60f1d8d5860a0e7f9914dda2166e70
JPS Grooves <jamescorvall@pm.me>
2026-01-08 21:55:12 -0500

    🍧 Live deploy

--- assets/antAttack-BVyt2yV0.js
+++ assets/antAttack-BVyt2yV0.js
@@ -1,4 +1,4 @@
-import{a as hn}from"./index-DDSNW7Bw.js";function Pt(a){if(a===void 0)throw new ReferenceError("this hasn't been initialised - super() hasn't been called");return a}function ur(a,t){a.prototype=Object.create(t.prototype),a.prototype.constructor=a,a.__proto__=t}/*!
+import{a as hn}from"./index-B2oAnVJh.js";function Pt(a){if(a===void 0)throw new ReferenceError("this hasn't been initialised - super() hasn't been called");return a}function ur(a,t){a.prototype=Object.create(t.prototype),a.prototype.constructor=a,a.__proto__=t}/*!
  * GSAP 3.14.2
  * https://gsap.com
  *
